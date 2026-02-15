96f2988789eb56ddb80268cd1cd4aaf356ca5854
Creativitylevel0 <parihargaurav1999@gmail.com>
2026-02-15 20:33:37 +0530

    Adding Multiple intermediate(Silver) marts,seeds,macros code

--- taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -1,32 +1,33 @@
 with tripdata as (
-  select *
-  from {{ source('raw_data','yellow_tripdata') }}
+   select *
+   from {{ source('raw_data','yellow_tripdata') }}
   where vendorid is not null 
 ),
 
 renamed as (
   select
-      identifiers
+      -- identifiers (Standardized naming- Same in both yellow/green taxi)
       cast(vendorid as integer) as vendor_id,
       cast(ratecodeid as integer) as ratecode_id,
       cast(pulocationid as integer) as pickup_location_id,
       cast(dolocationid as integer) as dropoff_location_id,
       
-      timestamps
+      -- timestamps
       cast(tpep_pickup_datetime as timestamp) as pickup_datetime,
       cast(tpep_dropoff_datetime as timestamp) as dropoff_datetime,
-      
-      trip info
+      1 as trip_type, --yellow taxis can only be street hail (since column is missing in yellow we created it to unionize the data of both green  and yellow taxi)
+      -- trip info
       store_and_fwd_flag,
       cast(passenger_count as integer) as passenger_count,
       cast(trip_distance as numeric) as trip_distance,
       
-      payment info
+      -- payment info
       cast(fare_amount as numeric) as fare_amount,
       cast(extra as numeric) as extra,
       cast(mta_tax as numeric) as mta_tax,
       cast(tip_amount as numeric) as tip_amount,
       cast(tolls_amount as numeric) as tolls_amount,
+      0 as ehail_fee, --Yellow taxi doesn't have ehail_fee 
       cast(improvement_surcharge as numeric) as improvement_surcharge,
       cast(total_amount as numeric) as total_amount,
       cast(payment_type as integer) as payment_type,
@@ -34,4 +35,5 @@ renamed as (
 )
 
 select * from renamed
+
 
